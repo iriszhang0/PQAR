b022aa1ec2597835416220c7e46af50a7dfdbec3
Iris Zhang <iriszhang0010@gmail.com>
2024-05-19 17:41:43 -0400

    Add Lecture 1 R file

---
+++ Lecture1.R
@@ -0,0 +1,12 @@
+###########################################
+### Title: Lecture 1 
+### Author: Iris Zhang 
+### Date Created: June 11th 2024
+### Last Modified: June 11th 2024
+###########################################
+
+
+
+
+
+
